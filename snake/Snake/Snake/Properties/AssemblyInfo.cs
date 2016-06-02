07f217f3f5899ec5087db6123ac188168d10939d
AnnA <anna.kozubets@gmail.com>
2016-06-02 15:09:56 +0300

    Hello world

--- snake/Snake/Snake/Properties/AssemblyInfo.cs
+++ snake/Snake/Snake/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3f7e3689-f56d-4a2a-9c7d-da90a7d300c8")]
+[assembly: Guid("82baff75-bee6-43b8-8aaa-fb27d20685f0")]
 
 // Version information for an assembly consists of the following four values:
 //
